be4e4b3489aeb917b2bc9c63f5e60b74b3dcb6a8
MORISHIMA Masahito <morishima@pnop.co.jp>
2022-08-22 18:34:03 +0900

    Add queue

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 | サンプル| 内容 | 
 |--|--| 
-| cron   | Cron バインディングのサンプル |
+| cron    | Cron バインディングのサンプル |
 | cron2   | Cron バインディング（HTTP POST 経由）のサンプル |
-| storage | BLOB Storage バインディングのサンプル | 
+| queue   | Storage Queue バインディングのサンプル |
+| storage | Storage BLOB  バインディングのサンプル | 
 | state   | Redisを利用した 状態管理のサンプル |
 | state2  | 同時実行制御の状態管理サンプル |
--- queue/main.go
+++ queue/main.go
@@ -1,5 +1,63 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	dapr "github.com/dapr/go-sdk/client"
+	"github.com/dapr/go-sdk/service/common"
+	daprd "github.com/dapr/go-sdk/service/grpc"
+)
+
 func main() {
 
+	ctx := context.Background()
+	client, err := dapr.NewClient()
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	// Enqueue
+	for i := 0; i < 10; i++ {
+
+		msg := fmt.Sprintf("Hello dapr queue = %d", i)
+		req := &dapr.InvokeBindingRequest{
+			Name:      "myqueue",
+			Operation: "create",
+			Data:      []byte(msg),
+		}
+		err = client.InvokeOutputBinding(ctx, req)
+
+		if err != nil {
+			panic(err)
+		}
+		log.Println("send message")
+	}
+
+	// Dequeue
+	s, err := daprd.NewService(":5005")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := s.AddBindingInvocationHandler("myqueue", runHandler); err != nil {
+		log.Fatalf("error adding binding handler: %v", err)
+	}
+
+	if err := s.Start(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("error starting service: %v", err)
+	}
+}
+
+func runHandler(ctx context.Context, in *common.BindingEvent) (out []byte, err error) {
+
+	log.Printf("Handler was invoked\n")
+	//log.Printf("Binding - Metadata:%v, Data:%v", in.Metadata, in.Data)
+	log.Printf("%s", string(in.Data))
+
+	return nil, nil
 }
